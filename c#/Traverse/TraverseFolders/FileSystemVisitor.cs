b858f0a2a35f6a589ec50707053db92ca62f7854
diana.martyn <diana.martyn@ihsmarkit.com>
2023-05-17 14:37:36 +0300

    traverse

--- c#/Traverse/TraverseFolders/FileSystemVisitor.cs
+++ c#/Traverse/TraverseFolders/FileSystemVisitor.cs
@@ -1,72 +1,37 @@
-public delegate bool ReturnedDelegate(string filename, string extention);
-
+public delegate bool ReturnedDelegate(string filename, string filterString);
 class FileSystemVisitor
 {
+    public readonly string Path;
+    public readonly ReturnedDelegate filterFunc;
     public FileSystemVisitor(string filename)
     {
-        foreach (string file in Explore(filename)) {
-            Console.WriteLine(file);
-        }
+        Path = filename;
+        filterFunc = (_, _) => true;
     }
 
     public FileSystemVisitor(string filename, ReturnedDelegate delegat)
     {
-        foreach (string file in Explore(filename,delegat)) {
-            Console.WriteLine(file);
-        }
-    }
-    
-    public IEnumerable<string> Explore(string path, ReturnedDelegate delegat)
-    {
-        foreach (var entry in Directory.EnumerateFileSystemEntries(path))
-        {
-            if (delegat(entry,"txt"))
-            {
-                yield return ShortenPath(entry);
-            }
-
-        }
-        
-        foreach (var subDir in Directory.EnumerateDirectories(path))
-        {
-            Console.WriteLine(ShortenPath(subDir));
-            foreach (var entry in Explore(subDir))
-            {
-                yield return ShortenPath(entry);
-            }
-        }
+        Path = filename;
+        filterFunc = delegat;
     }
     public IEnumerable<string> Explore(string path)
     {
         foreach (var entry in Directory.EnumerateFileSystemEntries(path))
         {
-            if (Filter(entry,"txt"))
+            if (filterFunc(entry,"txt"))
             {
-                yield return ShortenPath(entry);
+                yield return entry;
             }
 
         }
         
         foreach (var subDir in Directory.EnumerateDirectories(path))
         {
-            Console.WriteLine(ShortenPath(subDir));
+            Console.WriteLine(subDir);
             foreach (var entry in Explore(subDir))
             {
-                yield return ShortenPath(entry);
+                yield return entry;
             }
         }
-    }
-    
-    static string ShortenPath(string filename)
-    {
-        string[] newStr = filename.Split("/");
-
-        return newStr[newStr.Length -1];
-    }
-
-    bool Filter(string filename, string extention)
-    {
-        string[] newstring = ShortenPath(filename).Split(".");
-        return newstring[newstring.Length -1] == extention;
     }
 }
--- c#/Traverse/TraverseFolders/Program.cs
+++ c#/Traverse/TraverseFolders/Program.cs
@@ -6,17 +6,23 @@ namespace Traversing
     {
         static void Main(string[] args)
         {
-            var fileSystemVisitor1 = new FileSystemVisitor("../../../../testFolder");
-            
-            ReturnedDelegate filter = delegate(string filename, string extention)
+            var visitor = new FileSystemVisitor("../../../../testFolder");
+            var visitor2 = new FileSystemVisitor("../../../../testFolder",FilterByName);
+            Console.WriteLine("===== Result from first visitor ======");
+            foreach (var file in visitor.Explore("../../../../testFolder"))
             {
-                string[] newStr = filename.Split("/");
-                string[] newstring = newStr[newStr.Length -1].Split(".");
-                return newstring[newstring.Length -1] == extention;
-            };
-            var fileSystemVisitor2 = new FileSystemVisitor("../../../../testFolder", filter);
+                Console.WriteLine(file);
+            }
+            Console.WriteLine("===== Result from second visitor ======");
+            foreach (var file in visitor2.Explore("../../../../testFolder"))
+            {
+                Console.WriteLine(file);
+            }
         }
-        
+        static bool FilterByName(string filename,string filterString = "cs")
+        {
+            return filename.Contains(filterString);
+        }
     }
 
 }
