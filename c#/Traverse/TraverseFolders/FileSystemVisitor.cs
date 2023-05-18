90800030e9302d2bcf283345f73cadcd7b886ed3
diana.martyn <diana.martyn@ihsmarkit.com>
2023-05-18 15:41:29 +0300

    task2

--- c#/Traverse/TraverseFolders/FileSystemVisitor.cs
+++ c#/Traverse/TraverseFolders/FileSystemVisitor.cs
@@ -1,33 +1,63 @@
-public delegate bool ReturnedDelegate(string filename);
+public delegate bool FilterFunctionDelegate(string name);
+
+public delegate void Notify();
+
+public delegate bool MyCustomEvent(string filename,CustomEventArgs eventArgs);
+
 class FileSystemVisitor
 {
-    public readonly string Path;
-    public readonly ReturnedDelegate filterFunc;
+    private readonly string _path;
+    private readonly FilterFunctionDelegate _filterFunc;
+    private readonly CustomEventArgs _eventArgs;
+
+    public event Notify OnStart;
+    public event Notify OnFinish;
+    public event MyCustomEvent OnFileFound;
+
     public FileSystemVisitor(string filename)
     {
-        Path = filename;
-        filterFunc = (_) => true;
+        _path = filename;
+        _filterFunc = (_) => true;
+        OnStart = () => Console.WriteLine("Process started");
+        OnFinish = () => Console.WriteLine("Process finished");
     }
 
-    public FileSystemVisitor(string filename, ReturnedDelegate delegat)
+    public FileSystemVisitor(string filename, FilterFunctionDelegate delegat)
     {
-        Path = filename;
-        filterFunc = delegat;
+        _path = filename;
+        _filterFunc = delegat;
+        OnStart = () => Console.WriteLine("Process started");
+        OnFinish = () => Console.WriteLine("Process finished");
     }
+    
+    public FileSystemVisitor(string filename, FilterFunctionDelegate delegat, Notify started, Notify finished, MyCustomEvent fileFoundEvent)
+    {
+        _path = filename;
+        _filterFunc = delegat;
+        OnStart = started;
+        OnFinish = finished;
+        OnFileFound = fileFoundEvent;
+    }
+    
     public IEnumerable<string> Explore(string path)
     {
+        OnStart();
         foreach (var entry in Directory.EnumerateFileSystemEntries(path))
         {
-            if (filterFunc(entry))
+            if (OnFileFound(entry, _eventArgs))
+            {
+                break;
+            };
+            
+            if (_filterFunc(entry))
             {
                 yield return entry;
             }
-
         }
         
         foreach (var subDir in Directory.EnumerateDirectories(path))
         {
-            if (filterFunc(subDir))
+            if (_filterFunc(subDir))
             {
                 yield return subDir;
             }
@@ -41,5 +71,12 @@ class FileSystemVisitor
                 yield return entry;
             }
         }
+        OnFinish();
     }
+}
+
+public class CustomEventArgs : EventArgs
+{
+    public bool AllowAbort { get; set; }
+    
 }
--- c#/Traverse/TraverseFolders/Program.cs
+++ c#/Traverse/TraverseFolders/Program.cs
@@ -6,23 +6,70 @@ namespace Traversing
     {
         static void Main(string[] args)
         {
+            /*
             var visitor = new FileSystemVisitor("../../../../testFolder");
-            var visitor2 = new FileSystemVisitor("../../../../testFolder",FilterByCsExtension);
+            
             Console.WriteLine("===== Result from first visitor ======");
             foreach (var file in visitor.Explore("../../../../testFolder"))
             {
                 Console.WriteLine(file);
             }
+            
+            var visitor2 = new FileSystemVisitor("../../../../testFolder",FilterByCsExtension);
+            
             Console.WriteLine("===== Result from second visitor ======");
+            
             foreach (var file in visitor2.Explore("../../../../testFolder"))
             {
                 Console.WriteLine(file);
             }
+            */
+            var visitor3 = new FileSystemVisitor("../../../../testFolder",FilterByCsExtension,ShowStartMessage,ShowFinishMessage,FileFoundEvent);
+            
+            foreach (var file in visitor3.Explore("../../../../testFolder"))
+            {
+                Console.WriteLine(file);
+            }
         }
-        static bool FilterByCsExtension(string filename)
+
+        static bool FileFoundEvent(string file,CustomEventArgs args)
         {
-            return filename.Contains("cs");
+            var abortionFile = "../../../../testFolder/file1.cs";
+            var e = new CustomEventArgs();
+            if (file == abortionFile)
+            {
+                Console.WriteLine("File:" + file + " found. Abortion...");
+                e.AllowAbort = true;
+            }
+            return e.AllowAbort;
         }
+        
+        /*static void DirectoryFoundEvent(string dir,CustomEventArgs args)
+        {
+            var abortionFile = "../../../../testFolder/testFolder1cs";
+            
+            if (dir == abortionFile)
+            {
+                Console.WriteLine("Directory:" + dir + " found. Abortion...");
+                args.AllowAbort = true;
+            }
+        }*/
+        
+        static bool FilterByCsExtension(string name)
+        {
+            return name.Contains("cs");
+        }
+
+        static void ShowStartMessage()
+        {
+            Console.WriteLine("Process started");
+        }
+        
+        static void ShowFinishMessage()
+        {
+            Console.WriteLine("Process finished");
+        }
+        
     }
 
 }
