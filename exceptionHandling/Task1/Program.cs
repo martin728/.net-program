fefe4ca751bd955e95aa3990aab917af0a013740
diana.martyn <diana.martyn@ihsmarkit.com>
2023-05-25 13:04:24 +0300

    exception

---
+++ exceptionHandling/Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Task2;
+
+namespace Task1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Console.WriteLine("Enter");
+            // var input = Console.ReadLine();
+            // if (string.IsNullOrEmpty(input))
+            // {
+            //     throw new ArgumentOutOfRangeException(nameof(input), "Input length must be positive.");
+            // }
+            // Console.WriteLine(input.Substring(0, 1));
+
+            var num = new NumberParser();
+            num.Parse("-2147483648");
+        }
+    }
+}
---
+++ exceptionHandling/Task2/NumberParser.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Task2
+{
+    public class NumberParser : INumberParser
+    {
+        public int Parse(string stringValue)
+        {
+            if (stringValue is null)
+            {
+                throw new ArgumentNullException(nameof(stringValue),"Value is null");
+
+            }
+            var str = stringValue.Trim();
+
+            if (string.IsNullOrWhiteSpace(str) || string.IsNullOrEmpty(str))
+            {
+                throw new FormatException("Invalid input format");
+            }
+            
+            var num = 0;
+            var sign = 1;
+            int i = 0;
+            int res = 0;
+            if (str[0] == '-')
+            {
+                sign = -1;
+                i = 1;
+            }
+            if (str[0] == '+')
+            {
+                sign = 1;
+                i = 1;
+            }
+            
+            while (i < str.Length)
+            {
+                int currentNum = str[i] - '0';
+                if (currentNum > 9 || currentNum < 0)
+                {
+                    throw new FormatException("Input must contain only digits");
+                }
+
+                num = (num * 10) + currentNum;
+                i++;
+            }
+
+            res = num * sign;
+            
+            if (res > Int32.MaxValue || res < int.MinValue)
+            {
+                throw new OverflowException("Overflow exception");
+            }
+
+            Console.WriteLine(res);
+            return res;
+        }
+    }
+}
