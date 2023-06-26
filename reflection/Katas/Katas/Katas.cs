ab85430cfeff422290b388bc28993555d434326a
diana.martyn <diana.martyn@ihsmarkit.com>
2023-06-26 12:43:17 +0300

    OddEven unit tests

--- reflection/Katas/Katas/Katas.cs
+++ reflection/Katas/Katas/Katas.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Linq;
 
 namespace Katas
 {
     public class Katas
     {
         //The FizzBuzz Kata
-        public string FizzBuzz(int number) => 
-            number % 15 == 0 ? "FizzBuzz" : 
+        public string FizzBuzz(int number) =>
+            number % 15 == 0 ? "FizzBuzz" :
             number % 3 == 0 ? "Fizz" :
             number % 5 == 0 ? "Buzz" :
             number.ToString();
+
+        //The OddEven Kata
+        public string CheckNumber(int num)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- reflection/Katas/KatasTest/Tests.cs
+++ reflection/Katas/KatasTest/Tests.cs
@@ -13,7 +13,7 @@ namespace KatasTest
         {
             _fizzbuzz = new Katas.Katas();
         }
-
+        //FixxBuzz
         [TestCase(1, "1")]
         [TestCase(3, "Fizz")]
         [TestCase(5, "Buzz")]
@@ -23,8 +23,38 @@ namespace KatasTest
         [TestCase(98, "98")]
         public void FizzBuzz_WithVariousInputs_ReturnsExpectedOutput(int number, string expected)
         {
+            // Act
             string result = _fizzbuzz.FizzBuzz(number);
+            
+            // Assert
             Assert.AreEqual(expected, result);
         }
+        //
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "3")]
+        [TestCase(4, "Even")]
+        [TestCase(5, "5")]
+        [TestCase(6, "Even")]
+        [TestCase(7, "7")]
+        [TestCase(8, "Even")]
+        [TestCase(9, "Odd")]
+        [TestCase(10, "Even")]
+        [TestCase(11, "11")]
+        [TestCase(12, "Even")]
+        [TestCase(13, "13")]
+        [TestCase(14, "Even")]
+        [TestCase(15, "Odd")]
+        [TestCase(16, "Even")]
+        [TestCase(17, "17")]
+        [TestCase(18, "Even")]
+        [TestCase(19, "19")]
+        [TestCase(20, "Even")]
+        public void CheckNumber_ReturnsExpectedResult(int num, string expectedResult)
+        {
+            string result = _fizzbuzz.CheckNumber(num);
+            Assert.AreEqual(expectedResult, result);
+        }
+        
     }
 }
