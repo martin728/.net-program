987f5b082ca341619fa011504e5d477b52d0b6a8
diana.martyn <diana.martyn@ihsmarkit.com>
2023-09-07 20:33:10 +0300

    http task

---
+++ http/ClientApplication/ClientApplication/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string[] urls = {
+            "http://localhost:8888/MyName/",
+            "http://localhost:8888/Information/",
+            "http://localhost:8888/Success/",
+            "http://localhost:8888/Redirection/",
+            "http://localhost:8888/ClientError/",
+            "http://localhost:8888/ServerError/",
+            "http://localhost:8888/MyNameByHeader/",
+            "http://localhost:8888/MyNameByCookies/"
+        };
+
+        using (HttpClient client = new HttpClient(new HttpClientHandler() { UseCookies = false }))
+        {
+            foreach (string url in urls)
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        Console.WriteLine($"URL: {url}, Status Code: {(int)response.StatusCode}");
+                        if (url.EndsWith("/MyNameByHeader/"))
+                        {
+                            if (response.Headers.TryGetValues("X-MyName", out var headerValues))
+                            {
+                                string nameFromHeader = headerValues.GetEnumerator().Current;
+                                Console.WriteLine($"Name is: {nameFromHeader}");
+                            }
+                        }
+                        else if (url.EndsWith("/MyNameByCookies/"))
+                        {
+                            string cookieHeader = response.Headers.GetValues("Set-Cookie").ToString();
+                            string[] cookieParts = cookieHeader.Split(';');
+                            foreach (string part in cookieParts)
+                            {
+                                if (part.Trim().StartsWith("MyName="))
+                                {
+                                    string[] nameParts = part.Trim().Split('=');
+                                    if (nameParts.Length == 2)
+                                    {
+                                        Console.WriteLine($"Name is: {nameParts[1]}");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine($"URL: {url}, Error: {(int)response.StatusCode}");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"URL: {url}, Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
---
+++ http/ListenerApplication/ListenerApplication/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string url = "http://localhost:8888/";
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add(url);
+
+        Console.WriteLine("Listening for incoming requests...");
+        listener.Start();
+
+        while (true)
+        {
+            HttpListenerContext context = listener.GetContext();
+            ThreadPool.QueueUserWorkItem((ctx) =>
+            {
+                HandleRequest(ctx as HttpListenerContext);
+            }, context);
+        }
+    }
+
+    static void HandleRequest(HttpListenerContext context)
+    {
+        HttpListenerRequest request = context.Request;
+        HttpListenerResponse response = context.Response;
+
+        string resourcePath = request.Url.Segments[1].TrimEnd('/');
+
+        switch (resourcePath)
+        {
+            case "MyName":
+                SendResponse(response, HttpStatusCode.OK, "Diana");
+                break;
+            case "Information":
+                SendResponse(response, HttpStatusCode.Continue, "Information Response");
+                break;
+            case "Success":
+                SendResponse(response, HttpStatusCode.OK, "Success Response");
+                break;
+            case "Redirection":
+                SendResponse(response, HttpStatusCode.Redirect, "Redirection Response");
+                break;
+            case "ClientError":
+                SendResponse(response, HttpStatusCode.BadRequest, "Client Error Response");
+                break;
+            case "ServerError":
+                SendResponse(response, HttpStatusCode.InternalServerError, "Server Error Response");
+                break;
+            case "MyNameByHeader":
+                GetMyNameByHeader(response);
+                break;
+            case "MyNameByCookies":
+                GetMyNameByCookies(response);
+                break;
+            default:
+                SendResponse(response, HttpStatusCode.NotFound, "Resource not found.");
+                break;
+        }
+
+        response.Close();
+    }
+
+    static void SendResponse(HttpListenerResponse response, HttpStatusCode statusCode, string message)
+    {
+        response.StatusCode = (int)statusCode;
+        response.ContentType = "text/plain";
+        byte[] buffer = System.Text.Encoding.UTF8.GetBytes(message);
+        response.ContentLength64 = buffer.Length;
+        response.OutputStream.Write(buffer, 0, buffer.Length);
+    }
+
+    static void GetMyNameByHeader(HttpListenerResponse response)
+    {
+        response.Headers.Add("X-MyName", "Diana");
+        SendResponse(response, HttpStatusCode.OK, "Your Name (via Header)");
+    }
+
+    static void GetMyNameByCookies(HttpListenerResponse response)
+    {
+        response.Cookies.Add(new Cookie("MyName", "Diana"));
+        SendResponse(response, HttpStatusCode.OK, "Your Name (via Cookies)");
+    }
+}
